a78e7df457dfe7da862079c371e749a29a15b0b7
lnigri <80276613+lnigri@users.noreply.github.com>
2023-04-24 14:46:11 -0400

    Update HashMap.h

--- src/HashMap.h
+++ src/HashMap.h
@@ -3,14 +3,18 @@
 #include <list>
 #include <sstream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 class HashMap {
+    // hash node nested struct
     struct HashNode { 
         string key;
         int value;
         HashNode* next;
+
+        // constructor for hash node
         HashNode(string word){
             word = key;
             value = 1;
@@ -19,12 +23,38 @@ class HashMap {
     };
 
 private:
+    // HashMap with 1000 spots in the list
     list<HashNode*> table[1000];
-    int hashFunction(string key) {
-        int hashValue = 0;
-        for (int i = key.size(); i > 0; i--)
-            hashValue = hashValue * 31 + key[i];
+
+    // hash function that returns powers of 31 with ASCII 
+    unsigned int hashFunction(string key) {
+        unsigned int hashValue = 0;
+        for (int i = key.size() - 1; i >= 0; i--)
+            hashValue += key[i] * pow(31,i);
         return hashValue % 1000;
+    }
+
+    // insert hash node into hash map
+    HashMap insertHashNode(string word) {
+        int index = hashFunction(word);
+        bool found = false;
+
+        // iterate over each element in list at the index
+        // seperate chaining method
+        for (auto& node : table[index]) {
+            // increase the value of the word if found
+            if (node->key == word) {
+                node->value++;
+                found = true;
+                break;
+            }
+        }
+
+        // add node to hash map if it does not exist yet
+        if (!found) {
+            HashNode* node = new HashNode(word);
+            table[index].push_back(node);
+        }
     }
 
 public:
@@ -41,23 +71,11 @@ public:
 
     // destrcutor
     ~HashMap() {
+        // loop through table and iterate through each bucket list
         for (int i = 0; i < 1000; i++) {
             for (auto& node : table[i]) {
                 delete node;
             }
         }
     }
-
-    HashMap insertHashNode(string word) {
-        int index = hashFunction(word);
-        for (auto& node : table[index]) {
-            if (node->key == word) {
-                node->value++;
-                return;
-            }
-        }
-        HashNode* node = new HashNode(word);
-        table[index].push_back(node);
-    }
-    
 };
