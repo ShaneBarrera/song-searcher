daefb4b88d4f137663a09fd7a617896d070fad77
lnigri <80276613+lnigri@users.noreply.github.com>
2023-04-24 14:09:44 -0400

    Update HashMap.h

--- src/HashMap.h
+++ src/HashMap.h
@@ -1,45 +1,57 @@
 #pragma once
-#include <string>
 #include <iostream>
 #include <list>
+
 using namespace std;
 
 class HashMap {
-    struct HashNode {
+    struct HashNode { 
         string key;
         int value;
         HashNode* next;
-        HashNode(string key) {
-            this->key = key;
-            value = 0;
+        HashNode(string word){
+            word = key;
+            value = 1;
             next = nullptr;
         }
     };
 
 private:
-    list<HashNode> table[100];
-    int getHash(string key) {
+    list<HashNode*> table[1000];
+    int hashFunction(string key) {
         int hashValue = 0;
         for (int i = key.size(); i > 0; i--)
             hashValue = hashValue * 31 + key[i];
-        return hashValue % 100;
+        return hashValue % 1000;
     }
 
 public:
     // constructor
-    HashMap(string lyrics) {
-        while (lyrics.size() > 0) {
-            //if (lyrics.find(" "))
-            string word = lyrics.substr(0, (lyrics.find(" ")));
+    HashMap(string key) {
+        HashNode* node = new HashNode(key);
+        int index = hashFunction(key);
+        table[index].push_back(node);
+    }
 
+    // destrcutor
+    ~HashMap() {
+        for (int i = 0; i < 1000; i++) {
+            for (auto& node : table[i]) {
+                delete node;
+            }
         }
     }
 
-    // destructor
-    ~HashMap() {
-
+    HashMap insertHashNode(string word) {
+        int index = hashFunction(word);
+        for (auto& node : table[index]) {
+            if (node->key == word) {
+                node->value++;
+                return;
+            }
+        }
+        HashNode* node = new HashNode(word);
+        table[index].push_back(node);
     }
     
 };
-
-// insert hash(key, value);
