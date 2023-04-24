0aa3dedbe6ffed659bc37bd98bf3cafa44acf789
ShaneBarrera <122234259+ShaneBarrera@users.noreply.github.com>
2023-04-24 14:18:52 -0400

    Add files via upload

---
+++ src/TopFive.cpp
@@ -0,0 +1,22 @@
+#include "TopFive.h"
+#include <algorithm>
+
+void TopFive::insertWords(Song& song)
+{
+    string word;
+    istringstream stream(song.lyrics);
+    while (stream >> word)
+    {
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        if (song.wordMapHash.find(word) == song.wordMapHash.end()) {
+            song.wordMapHash[word] = 1;
+        } 
+        else {
+            //  We will increment for the key to store the next word
+            song.wordMapHash[word]++;
+        }
+
+        // For the RB tree, insert the current word
+        song.wordMapTree.Insert(word);
+    }
+}
