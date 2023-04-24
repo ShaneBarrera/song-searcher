7271852773a8336ffd7c1352e0c1e3ee50df08ce
ShaneBarrera <122234259+ShaneBarrera@users.noreply.github.com>
2023-04-24 16:22:35 -0400

    Update TopFive.cpp

--- src/TopFive.cpp
+++ src/TopFive.cpp
@@ -5,33 +5,53 @@ void TopFive::CreateMapsForSongs(vector<Song> allSongs)
 {
     for (auto& song : allSongs)
     {
-        insertWords(song); //  The insertWords(song) function calls the constructor for both tree and hash
+        insertWordsHash(song); //  The insertWords(song) function calls the constructor for both tree and hash
+        insertWordsTree(song);
     }
 }
 
-void TopFive::PrintTop5(string searchedWord)
+void TopFive::PrintTop5Hash(string searchedWord) 
 {
-    vector<Song> topFive = FindTop5(); //  Haven't created the FindTop5() function yet
-    if (topFive.empty())
+    vector<Song> topFive = FindTop5Hash(allSongs, searchedWord); //  Haven't created the FindTop5() function yet
+    if (topFive.empty()) 
     {
         return;
     }
 
     cout << "\"" << searchedWord << "\" appears in the following songs most frequently:\n";
     int rank = 1;
-    for (auto& song : topFive) {
-        cout << rank << ". \"" << song.name << "\" " << song.wordMapHash.getWordFequency(searchedWord)
+        for (auto& song : topFive) {
+            cout << rank << ". \"" << song.name << "\" " << song.wordMapHash.getWordFequency(searchedWord)
             << " " << song.streams << endl;
-        ++rank;
+            ++rank;
+        }
+}
+
+void TopFive::PrintTop5Tree(string searchedWord) 
+{
+    vector<Song> topFive = FindTop5Tree(allSongs, searchedWord); //  Haven't created the FindTop5() function yet
+    if (topFive.empty()) 
+    {
+        return;
     }
 
     cout << "\"" << searchedWord << "\" appears in the following songs most frequently:\n";
     int rank = 1;
-    for (auto& song : topFive) {
-        cout << rank << ". \"" << song.name << "\" " << song.wordMapTree.GetNumUses(searchedWord)
+        for (auto& song : topFive) {
+            cout << rank << ". \"" << song.name << "\" " << song.wordMapTree.GetNumUses(searchedWord)
             << " " << song.streams << endl;
-        ++rank;
-    }
+            ++rank;
+        }
+}
+
+void TopFive::insertWordsHash(Song& song)
+{
+    HashMap(song.lyrics);
+}
+
+void TopFive::insertWordsTree(Song& song)
+{
+    TreeMap(song.lyrics);
 }
 
 vector<TopFive::Song> TopFive::FindTop5Hash(vector<Song>& allSongs, string word) {
@@ -58,9 +78,9 @@ vector<TopFive::Song> TopFive::FindTop5Tree(vector<Song>& allSongs, string word)
 
 }
 
-
-void TopFive::insertWords(Song& song)
-{
-    TreeMap(song.lyrics);
-    HashMap(song.lyrics);
-}
+//vect or sorted map
+//add them to a vector
+//sort
+//return top 5
+//keep track of the name of the word is for the number
+//Maybe an ordered map? or a max
