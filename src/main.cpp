dc9a573e681a79dba4bdde342071ffe6f2e371ea
Alex Johnson <alex@jeffjohnson.net>
2023-04-25 17:58:11 -0400

    Cleaned up main.cpp to make it more readable

--- src/main.cpp
+++ src/main.cpp
@@ -16,50 +16,35 @@ using namespace std;
 
 int main()
 {
-//    string sample = "This is an example with example is a multiple copies of this word";
-//    cout << sample << endl;
-//    TreeMap map = TreeMap(sample);
-//    map.PrintInorder(map.GetRoot());
-//    cout << map.GetNumUses("this") << endl;
-
+    /// Alex's verification, need your own if running most likely? but feel free to try
     bridges::Bridges bridges("exla", "1255981382944");
-
     bridges.setTitle("Song Data Stuff");
     bridges::DataSource ds(&bridges);
 
-//    bridges::dataset::Song s = ds.getSong("Harder Faster Better Stronger", "Daft Punk");
-//
-////     print song attributes
-//    cout << endl << "Title: " << s.getSongTitle() << endl
-//         << "Artist: " << s.getArtist() << endl
-//         << "Album: " << s.getAlbumTitle() << endl
-//         << "Release Date: " << s.getReleaseDate() << endl;
-//
-//    // print lyrics
-//    std::cout << s.getLyrics() << std::endl;
-
+    ///IMPORTANT NOTE: there are two Song classes in use: bridges::dataset::Song and TopFive::Song
+    //get the whole dataset of songs and store in a Song vector
     std::vector<bridges::dataset::Song> songs = ds.getSongData();
 
+    //construct an instance of our TopFive class
     TopFive top5 = TopFive();
 
-    cout<< songs[0].getLyrics() << endl;
-    TopFive::Song s(songs[0].getSongTitle(), songs[0].getArtist(), songs[0].getLyrics());
-//    top5.allSongs.push_back(s);
+    ///for testing (uncomment if you want to see how it works)
+//    //try printing lyrics and constructing a TopFive::Song object for a single Song from dataset
+//    cout<< songs[0].getLyrics() << endl;
+//    TopFive::Song s(songs[0].getSongTitle(), songs[0].getArtist(), songs[0].getLyrics());
 
-
-    // loop through the songs and print their titles
+    /// for all songs in the dataset make TopFive::Song objects for them, also add them to the allSongs vector
     for (bridges::dataset::Song song : songs) {
         TopFive::Song s(song.getSongTitle(), song.getArtist(), song.getLyrics());
         top5.allSongs.push_back(s);
     }
 
-    //print the num uses (using the tree) of the word "happy" in each song
-    for (auto song : top5.allSongs) {
-        cout << song.name << " uses the word happy : "<< song.wordMapTree.GetNumUses("happy") << endl;
-    }
-
+    ///use tree to print the top 5 songs with word "happy"
+    cout << "Using the red-black tree..." << endl;
     top5.PrintTop5Tree("happy");
 
+
+///old stuff, probably needs to be cleaned up or deleted?
     // TreeMap songTree(132);
     // unordered_map<string, int> frequency;
 
