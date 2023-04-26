c1b7b58e734d67bb9006549078c0737e464ee2a9
ShaneBarrera <122234259+ShaneBarrera@users.noreply.github.com>
2023-04-26 11:32:36 -0400

    Update main.cpp
    Ensure that user enters a real word with no space.

--- src/main.cpp
+++ src/main.cpp
@@ -14,96 +14,52 @@ using namespace std;
 
 int main()
 {
-    /// Alex's verification, need your own if running most likely? but feel free to try
+    //  Alex's verification, need your own if running most likely? but feel free to try
     bridges::Bridges bridges("exla", "1255981382944");
     bridges.setTitle("Song Data Stuff");
     bridges::DataSource ds(&bridges);
 
-    ///IMPORTANT NOTE: there are two Song classes in use: bridges::dataset::Song and TopFive::Song
-    //get the whole dataset of songs and store in a Song vector
+    //  IMPORTANT NOTE: there are two Song classes in use: bridges::dataset::Song and TopFive::Song
+    //  get the whole dataset of songs and store in a Song vector
     std::vector<bridges::dataset::Song> songs = ds.getSongData();
 
-    //construct an instance of our TopFive class
+    //  Construct an instance of our TopFive class
     TopFive top5 = TopFive();
 
-    ///for testing (uncomment if you want to see how it works)
-//    //try printing lyrics and constructing a TopFive::Song object for a single Song from dataset
-//    cout<< songs[0].getLyrics() << endl;
-//    TopFive::Song s(songs[0].getSongTitle(), songs[0].getArtist(), songs[0].getLyrics());
+    //  TESTING (uncomment if you want to see how it works)
+    //  Try printing lyrics and constructing a TopFive::Song object for a single Song from dataset
+    //cout<< songs[0].getLyrics() << endl;
+    //TopFive::Song s(songs[0].getSongTitle(), songs[0].getArtist(), songs[0].getLyrics());
 
-    /// for all songs in the dataset make TopFive::Song objects for them, also add them to the allSongs vector
+    //  For all songs in the dataset make TopFive::Song objects for them, also add them to the allSongs vector
     for (bridges::dataset::Song song : songs) {
         TopFive::Song s = TopFive::Song(song.getSongTitle(), song.getArtist(), song.getLyrics());
         top5.allSongs.push_back(s);
     }
-
+    
     TopFive::Song x = TopFive::Song("Sample Song", "Sample artist", "yo yo yo my sample lyrics are super happy");
     top5.allSongs.push_back(x);
 
-
-    ///use tree to print the top 5 songs with word "happy"
+    string input;
+    bool isWord = false;
+    
+    while (!isWord) {
+        cout << "Enter a word to search song lyrics for: " << endl;
+        getline(cin, input);
+        if (!input.empty() && (input.begin(), input.end(), ::isalpha)) {
+            isWord = true;
+        }
+        else {
+            cout << "Invalid input. Please enter another word with no spaces!" << endl;
+        }
+    }
+    
+    //use tree to print the top 5 songs with word "happy"
     cout << "Using the red-black tree..." << endl;
-    top5.PrintTop5Tree("happy");
+    top5.PrintTop5Tree(input);
 
     cout << "Using the hash..." << endl;
-    top5.PrintTop5Hash("happy");
-
-
-///old stuff, probably needs to be cleaned up or deleted?
-    // TreeMap songTree(132);
-    // unordered_map<string, int> frequency;
-
-    // for (int i = 0; i < songTree.size(); i++) 
-    // {
-    //     TreeMap* song = songTree.getSong(i);
-        
-    //     for (string word : song->getWords()) 
-    //     {
-    //         frequency[word]++;
-    //     }
-    // }
-
-    // while (true) 
-    // {
-    //     cout << "Enter a word to search song lyrics for: ";
-    //     string word;
-    //     getline(cin, word);
-
-    //     int num = frequency.find(word);
-    //     if (num == frequency.end())
-    //     {
-    //         cout << "That word doesn't exist. Please try again." << endl;
-    //         continue;
-    //     }
-
-    //     vector<TreeMap*> songList;
-    //     for (int i = 0; i < songTree.size(); i++) 
-    //     {
-    //         TreeMap* song = songTree.getSong(i);
-            
-    //         if (song->contains(word)) 
-    //         {
-    //             songList.push_back(song);
-    //         }
-    //     }
-
-    //     sort(songs.begin(), songs.end(), [](Song* a, Song* b) {
-    //         if (a->getWordFrequency(word) != b->getWordFrequency(word)) {
-    //             return a->getWordFrequency(word) > b->getWordFrequency(word);
-    //         } else {
-    //             return a->getStreams() > b->getStreams();
-    //         }
-    //     });
-
-    //     // print list of songs
-    //     cout << "\"" << word << "\" appears in the following songs most frequently:" << endl;
-    //     int rank = 1;
-    //     for (Song* song : songs) {
-    //         cout << rank << ". \"" << song->getTitle() << "\" " << song->getWordFrequency(word) << " " << song->getStreams() << endl;
-    //         rank++;
-    //     }
-    // }
-
+    top5.PrintTop5Hash(input);
     top5.DestroyEverything();
 
     return 0;
